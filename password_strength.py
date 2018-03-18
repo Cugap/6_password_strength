72b69e5ea86c330554f0244ec122057ac846fb69
Ruslan <rusiacugap@mail.ru>
2018-03-18 21:49:37 +0300

    Функция проверки наличия двух регистров не пашет

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,48 @@
+import re
 
+def HaveForbidenSymbols(password):
+    digitsList = re.findall(r"[^a-z0-9]", password, re.IGNORECASE)
+    if len(digitsList) > 0:
+        print ("Password have forbidden symbols")
+        return True
+    return False
+
+def HassUpperAndLowerCaseChars(password):
+    if ((password.isupper) and (password.islower)):
+        print ("Password has upper- and lower-case chars")
+        return True
+    return False
+
+def OnlyDigits(password):
+    digitsList = re.findall(r"[0-9]", password)
+    if len(digitsList) == len(password):
+        print ("password has only digits")
+        return True
+    return False
+
+def HassOneOrMoreDigits(password):
+    digitsList = re.findall(r"[0-9]", password)
+    if len(digitsList) > 0:
+        print ("Password has one or more digits")
+        return True
+    return False
 
 def get_password_strength(password):
+    if (HaveForbidenSymbols(password)):
+        return 0
+    strength = 1
+    if OnlyDigits(password):
+        return strength
+    strength += 1
+    if HassUpperAndLowerCaseChars(password):
+        strength += 1
+    if HassOneOrMoreDigits(password):
+        strength += 1
+    return strength
     pass
 
 
 if __name__ == '__main__':
-    pass
+    print ("Please input your password")
+    strengthLevel = get_password_strength(input())
+    print (str(strengthLevel))
